ac41d8af166ec3758e0cf41b153ba7db812cc986
Scotty Brown <scottybrown98@gmail.com>
2023-07-03 19:07:25 -0500

    Add createCard function

---
+++ src/card.js
@@ -0,0 +1,12 @@
+function createCard(id, question, answer, correctAnswer) {
+    return {
+        id: id,
+        question: question,
+        answers: answer,
+        correctAnswer: correctAnswer
+    }
+}
+
+module.exports = {
+    createCard,
+}
