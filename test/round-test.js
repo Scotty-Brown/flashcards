825e2adbc69a73a1b1f143c58163c55ea333b743
Scotty Brown <scottybrown98@gmail.com>
2023-07-05 23:38:05 -0500

    Add testing for endRound function

--- test/round-test.js
+++ test/round-test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 // const { createRound } = require('../src/round');
-const { createRound, takeTurn, calculatePercentCorrect } = require('../src/round');
+const { createRound, takeTurn, calculatePercentCorrect, endRound } = require('../src/round');
 const { createDeck } = require('../src/deck');
 const { createCard } = require('../src/card');
 
@@ -60,9 +60,30 @@ describe('calculate percentage', function() {
         incorrectGuesses: ['one', 'two']
     }
 
-    const percent = calculatePercentCorrect(roundObject)
+    const percent = calculatePercentCorrect(mockRound)
 
     it('should calculate percent total of correct guesses', function() {
         expect(percent).to.equal(80)
+    })
+})
+
+describe('end round', function() {
+    const card1 = createCard(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = createCard(2, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = createCard(3, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+    const card4 = createCard(4, 'What is Scotty\'s middle name?', ['lee', 'laa', 'loo'], 'lee');
+
+    const deck = createDeck([card1, card2, card3, card4])
+
+    const round = createRound(deck)
+    const turn1 = takeTurn('pug', round)
+    const turn2 = takeTurn('gallbladder', round)
+    const turn3 = takeTurn('playing with bubble wrap', round)
+    const turn4 = takeTurn('laa', round)
+
+    const endRoundSentence = endRound(round)
+console.log(endRoundSentence)
+    it('should return endRound statement with correct percentage', function() {
+        expect(endRoundSentence).to.equal('**Round Over!** You answered 50% of the questions correctly!')
     })
 })
